b89498ceedca15ea0f3f0f01614df4edd3cba2a0
Sean McCafferty <smccafferty@voceconnect.com>
2014-12-19 12:46:06 -0500

    Login via SSO using JS instead of outputting image tags.

--- inc/sso.php
+++ inc/sso.php
@@ -2,8 +2,6 @@
 if ( is_null( $sso ) || is_null( $action ) )
 	die;
 
-$network_sites = array_diff( WP_MultiSite_SSO::get_network_sites(), array( home_url() ) );
-
 $site_args = array(
 	'action' => $action
 );
@@ -11,20 +9,36 @@ $site_args = array(
 if ( !empty( $sso ) )
 	$site_args['sso'] = urlencode( $sso );
 
+$network_sites = array_diff( WP_MultiSite_SSO::get_network_sites(), array( home_url() ) );
+
+// add the site args to each site
+$network_sites = array_map( function( $network_site ) use ($site_args) {
+	return add_query_arg( $site_args, $network_site );
+}, $network_sites );
+
 ?>
 <html>
 	<head>
 		<script>
-		window.addEventListener("load", loadComplete, false);
+		function loadSite(site) {
+			var ssoImg = document.createElement("img");
+			ssoImg.setAttribute('src', site);
+		}
+		function seqLoadSites() {
+			var sites = <?php echo json_encode( array_values( $network_sites ) ); ?>;
+			for (site in sites){
+				loadSite(sites[site]);
+			}
+			loadComplete();
+		}
 		function loadComplete(){
 		   window.location="<?php echo home_url(); ?>";
 		}
+		window.addEventListener("load", seqLoadSites, false);
 		</script>
+		<?php do_action( 'sso_head' ); ?>
 	</head>
 	<body>
 		<p>Please wait.....</p>
-		<?php foreach( $network_sites as $network_site ) : ?>
-			<img src="<?php echo add_query_arg( $site_args, $network_site ); ?>" />
-		<?php endforeach; ?>
 	</body>
 </html>
